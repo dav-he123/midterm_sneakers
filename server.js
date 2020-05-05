b9877cebe98fd7bbf8705984c5dbdf8a8e231ca7
David <david.he07@yahoo.ca>
2020-05-05 15:32:57 -0400

    updated cookies for messages

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const PORT          = process.env.PORT || 8080;
-const ENV           = process.env.ENV || "development";
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const cookieParser  = require("cookie-parser");
-const sass          = require("node-sass-middleware");
-const app           = express();
-const morgan        = require('morgan');
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const sass = require("node-sass-middleware");
+const app = express();
+const morgan = require("morgan");
 
 //Routes
-const sneakersRouter = require('./routes/sneakers');
-const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login');
-const widgetsRouter = require('./routes/widgets');
+const sneakersRouter = require("./routes/sneakers");
+const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+const widgetsRouter = require("./routes/widgets");
 const cookieSession = require("cookie-session");
 const messagesRoutes = require("./routes/messages");
 
@@ -24,27 +24,29 @@ const messagesRoutes = require("./routes/messages");
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+app.use(
+  "/styles",
+  sass({
+    src: __dirname + "/styles",
+    dest: __dirname + "/public/styles",
+    debug: true,
+    outputStyle: "expanded",
+  })
+);
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(cookieSession({ name: "user_id", secret: "asdfg" }));
 
 // endpoints
-app.use('/sneakers', sneakersRouter);
-app.use('/users', usersRouter);
-app.use('/widgets', widgetsRouter);
-app.use('/login', loginRouter);
+app.use("/sneakers", sneakersRouter);
+app.use("/users", usersRouter);
+app.use("/widgets", widgetsRouter);
+app.use("/login", loginRouter);
 app.use("/messages", messagesRoutes);
-
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -64,5 +66,3 @@ app.get("/login", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT} 😎`);
 });
-
-
