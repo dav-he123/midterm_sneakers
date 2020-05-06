491668176c4f74cd7c053c18519ee6198c4618da
David <david.he07@yahoo.ca>
2020-05-06 10:31:44 -0400

    updated code for favourites

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(() => {
   // $.ajax({
   //   method: "GET",
@@ -10,54 +9,19 @@ $(() => {
   //   }
   // });
 
-  $("#message-form").submit(function(event) {
+  $("#message-form").submit(function (event) {
     event.preventDefault();
     const message = $(".message-text").val();
     console.log(message);
-
 
     $.ajax({
       url: `/messages`,
       type: "POST",
       dataType: "text",
-      data: {message_text: message}
-    })
-    .then((response) => {
-
+      data: { message_text: message },
+    }).then((response) => {
       // console.log()
       // $('.message-text').val('').change();
-
     });
-
-
-
-
-  })
-
+  });
 });
-=======
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users",
-//   }).done((users) => {
-//     console.log(users);
-//     console.log(typeof users);
-//     for (user of users) {
-//       $("<div>").text(user.email).appendTo($("body"));
-//     }
-//   });
-// });
-
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/favourites",
-//   }).done((favourites) => {
-//     console.log(favourites);
-//     for (let i of favourites) {
-//       $("<div>").text(i.item_id).appendTo($("body"));
-//     }
-//   });
-// });
->>>>>>> master
